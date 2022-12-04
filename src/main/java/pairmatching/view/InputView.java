239ac4f9db88e239fd42865fcaffade46dc8e37b
SEONGJOON KIM <jobcho6320@naver.com>
2022-12-04 15:18:28 +0900

    feat: 게임 실행 시 숫자 입력 메소드 구현

---
+++ src/main/java/pairmatching/view/InputView.java
@@ -0,0 +1,14 @@
+package pairmatching.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    private InputView() {
+    }
+
+    public static int inputStartNumber() {
+        return Integer.parseInt(Console.readLine());
+    }
+
+
+}
