b875ca9917e278fb556cff8bb76101a86cdac8ed
SEONGJOON KIM <jobcho6320@naver.com>
2022-12-04 15:17:22 +0900

    feat: main 실행 메소드 구현

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,10 @@
 package pairmatching;
 
+import pairmatching.controller.GameController;
+
 public class Application {
     public static void main(String[] args) {
-
+        GameController gameController = new GameController();
+        gameController.run();
     }
 }
